ee2fca25a7fa16496a9068190f97f8dca777b5d8
Quoc Ngo <quocnv@minori.com.vn>
2021-11-29 21:20:01 +0700

    push

--- andyngojs.github.io/crush-love/CONFIG.js
+++ andyngojs.github.io/crush-love/CONFIG.js
@@ -1,8 +1,9 @@
 const CONFIGDATA = {
-  titleHeader: "Bạn thấy mình là người thế nào *^_^*",
-  descriptionHeader: "Tiên",
+  titleHeader: "Bạn hãy tự nhận xét về bản thân mình --- Hạnh *^_^*",
+  descriptionHeader: "张氏美幸",
   buttonYes: "Dễ thương",
   buttonNo: "Hông dễ thương",
   titleModar: "Chúc mừng bạn đã lựa chọn đúng!",
-  descriptionModar: "You are so cuteeeee ＾▽＾",
+  descriptionModar:
+    "You are so cuteeeee, Chúc Hạnh ngủ thật ngon, Wǎn'ān em ＾▽＾",
 };
